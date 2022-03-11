15143e5a20d54e615e61b7f5305c1778c74b8411
Elin <elin.fritiofsson@gmail.com>
2022-03-11 16:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ fs.readdir(folder, (err, files)=> {
     if(err) throw err;
 
     files.forEach(file => {
-        console.log(file);
         let fullpath = path.join(folder, file);
-        console.log(fullpath);
         let extension = path.extname(fullpath);
-
         const extensionName = extension.substring(1);
-        console.log("extname " + extensionName);
-
+        
         if (!fs.existsSync(folder + `\\sorted_${extensionName}\\`)){
             fs.mkdirSync(folder + `\\sorted_${extensionName}\\`);
         }
